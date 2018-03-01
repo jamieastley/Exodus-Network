19d2cd8e92a37cf8a223dd23b766c8626b6b688f
Jamie <jastley92@gmail.com>
2018-03-02 02:06:30 +1100

    LFG model and post submission started

---
+++ app/src/main/java/com/jastley/warmindfordestiny2/LFG/LFGPost.java
@@ -0,0 +1,59 @@
+package com.jastley.warmindfordestiny2.LFG;
+
+/**
+ * Created by jastl on 2/03/2018.
+ */
+
+public class LFGPost {
+
+    private String activityTitle;
+    private String activityCheckpoint;
+    private String lightLevel;
+    private String membershipType;
+    private String displayName;
+    private String classType;
+    private String dateTime;
+
+    public LFGPost() {
+        //empty constructor for Firebase
+    }
+
+    public LFGPost(String activityTitle, String activityCheckpoint, String lightLevel, String membershipType, String displayName, String classType, String dateTime) {
+        this.activityTitle = activityTitle;
+        this.activityCheckpoint = activityCheckpoint;
+        this.lightLevel = lightLevel;
+        this.membershipType = membershipType;
+        this.displayName = displayName;
+        this.classType = classType;
+        this.dateTime = dateTime;
+    }
+
+
+    public String getActivityTitle() {
+        return activityTitle;
+    }
+
+    public String getActivityCheckpoint() {
+        return activityCheckpoint;
+    }
+
+    public String getLightLevel() {
+        return lightLevel;
+    }
+
+    public String getMembershipType() {
+        return membershipType;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public String getClassType() {
+        return classType;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+}
---
+++ app/src/main/java/com/jastley/warmindfordestiny2/LFG/NewLFGPostActivity.java
@@ -0,0 +1,14 @@
+package com.jastley.warmindfordestiny2.LFG;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.jastley.warmindfordestiny2.R;
+
+public class NewLFGPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_lfgpost);
+    }
+}
