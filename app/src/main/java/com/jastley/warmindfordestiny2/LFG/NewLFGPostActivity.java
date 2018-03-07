e31d681efcd5d404e9eac29e3baa584d4c83b260
Jamie <jastley92@gmail.com>
2018-03-07 17:49:46 +1100

    Testing FirebaseUI

--- app/src/main/java/com/jastley/warmindfordestiny2/LFG/NewLFGPostActivity.java
+++ app/src/main/java/com/jastley/warmindfordestiny2/LFG/NewLFGPostActivity.java
@@ -2,13 +2,61 @@ package com.jastley.warmindfordestiny2.LFG;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.FirebaseDatabase;
 import com.jastley.warmindfordestiny2.R;
 
-public class NewLFGPostActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class NewLFGPostActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText activityName;
+    private EditText activityCheckpoint;
+    private Button submitBtn;
+    private String key;
+    private String lightLevel;
+    private String membershipType;
+    private String displayName;
+    private String classType;
+    private String dateTime;
+    private static final FirebaseDatabase DATABASE = FirebaseDatabase.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_lfgpost);
+
+        activityName = findViewById(R.id.activity_name_input);
+        activityCheckpoint = findViewById(R.id.activity_checkpoint_input);
+        submitBtn = findViewById(R.id.submit_lfg_post_button);
+
+        lightLevel = "300";
+        membershipType = "2";
+        displayName = "Jeewwbacca";
+        classType = "1";
+        dateTime = Calendar.getInstance().getTime().toString();
+
+        submitBtn.setOnClickListener(this);
+
+        key = DATABASE.getReference().push().getKey();
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        LFGPost newPost = new LFGPost(activityName.getText().toString(),
+                                    activityCheckpoint.getText().toString(),
+                                    lightLevel, membershipType, displayName, classType, dateTime);
+
+        DATABASE.getReference().child("lfg").child(displayName).setValue(newPost);
+        finish();
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
